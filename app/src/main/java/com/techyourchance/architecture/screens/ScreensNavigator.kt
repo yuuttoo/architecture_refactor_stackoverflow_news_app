6dfa6275696bef1b24bdd194050ff643577e74c1
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-06-11 02:04:27 +0800

    refactor: Add ScreensNavigator: setNestedNavController & setParentNavController

--- app/src/main/java/com/techyourchance/architecture/screens/ScreensNavigator.kt
+++ app/src/main/java/com/techyourchance/architecture/screens/ScreensNavigator.kt
@@ -1,16 +1,31 @@
 package com.techyourchance.architecture.screens
 
 import android.os.Bundle
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import java.lang.RuntimeException
 
 class ScreensNavigator {
+
+    private val scope = CoroutineScope(Dispatchers.Main.immediate)//UI thread : immediate for instant update, better UI performance
+
 
     private lateinit var parentNavController: NavHostController
     private lateinit var nestedNavController: NavHostController
 
-    val currentBottomTab = MutableStateFlow<BottomTab>(BottomTab.Main)
+    private var nestedNavControllerObserveJob: Job? = null
+    private var parentNavControllerObserveJob: Job? = null
+
+
+    val currentBottomTab = MutableStateFlow<BottomTab?>(BottomTab.Main)
     val currentRoute = MutableStateFlow<Route?>(null)
     val isRootRoute = MutableStateFlow(false)
     val arguments = MutableStateFlow<Bundle?>(null)//for backstackEntryState但不想暴露，又必須給id,title
@@ -37,11 +52,44 @@ class ScreensNavigator {
         }
     }
 
-    fun setParentNavController(parentNavController: NavController) {
+    fun setParentNavController(navController: NavHostController) {
+        parentNavController = navController
+
+        parentNavControllerObserveJob?.cancel()
+        parentNavControllerObserveJob = scope.launch {
+            navController.currentBackStackEntryFlow.map { backStackEntry ->
+                val bottomTab = when (val routeName = backStackEntry.destination.route) {
+                    Route.MainTab.routeName -> BottomTab.Main
+                    Route.FavoritesTab.routeName -> BottomTab.Favorites
+                    null -> null
+                    else -> throw RuntimeException("unsupported bottom tab: $routeName")
+                }
+                currentBottomTab.value = bottomTab
+            }.collect()
+        }
 
     }
-    fun setNestedNavController(mainNestedNavController: NavController) {
+    fun setNestedNavController(navController: NavHostController) {
+        nestedNavController = navController
 
+        nestedNavControllerObserveJob?.cancel()//cancel if there was previous job existing
+
+        nestedNavControllerObserveJob = scope.launch {
+        navController.currentBackStackEntryFlow.map { backStackEntry ->
+            val route = when(val routeName = backStackEntry.destination.route) {
+                Route.MainTab.routeName -> Route.MainTab
+                Route.FavoritesTab.routeName -> Route.FavoritesTab
+                Route.QuestionsListScreen.routeName -> Route.QuestionsListScreen
+                Route.QuestionDetailsScreen.routeName -> Route.QuestionDetailsScreen
+                Route.FavoriteQuestionsScreen.routeName -> Route.FavoriteQuestionsScreen
+                null -> null
+                else -> throw RuntimeException("unsupported route: $routeName")
+            }
+            currentRoute.value = route
+            isRootRoute.value = route == Route.QuestionsListScreen
+            arguments.value = backStackEntry.arguments
+        }.collect()
+      }
     }
     companion object {
         val BOTTOM_TABS = listOf(BottomTab.Main, BottomTab.Favorites)
