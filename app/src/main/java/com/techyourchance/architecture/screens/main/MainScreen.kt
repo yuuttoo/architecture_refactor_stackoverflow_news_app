5fbffcebf3d28cc866248f6a405565392b636a11
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-06-11 02:58:32 +0800

    refactor: Add ScreensNavigator: fixed missed arguments

--- app/src/main/java/com/techyourchance/architecture/screens/main/MainScreen.kt
+++ app/src/main/java/com/techyourchance/architecture/screens/main/MainScreen.kt
@@ -150,7 +150,7 @@ private fun MainScreenContent(
                     }
                     composable(route = Route.QuestionDetailsScreen().routeName) { backStackEntry ->
                         QuestionDetailsScreen(
-                            questionId = backStackEntry.arguments?.getString("questionId")!!,
+                            questionId = (screensNavigator.currentRoute.collectAsState().value as Route.QuestionDetailsScreen).questionId,
                             stackoverflowApi = stackoverflowApi,
                             favoriteQuestionDao = favoriteQuestionDao,
                             onError = {
@@ -176,7 +176,7 @@ private fun MainScreenContent(
                     }
                     composable(route = Route.QuestionDetailsScreen().routeName) { backStackEntry ->
                         QuestionDetailsScreen(
-                            questionId = backStackEntry.arguments?.getString("questionId")!!,
+                            questionId = (screensNavigator.currentRoute.collectAsState().value as Route.QuestionDetailsScreen).questionId,
                             stackoverflowApi = stackoverflowApi,
                             favoriteQuestionDao = favoriteQuestionDao,
                             onError = {
